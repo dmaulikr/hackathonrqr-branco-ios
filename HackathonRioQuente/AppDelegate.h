2b60d642a3d7ee86e0a62fbe60efece9a3337a44
joselinoneto <joselinoneto@gmail.com>
2017-08-05 09:00:06 -0300

    Initial Commit

---
+++ HackathonRioQuente/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  HackathonRioQuente
+//
+//  Created by Jose Lino Neto on 05/08/17.
+//  Copyright © 2017 Construtor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ HackathonRioQuente/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackathonRioQuente
+//
+//  Created by Jose Lino Neto on 05/08/17.
+//  Copyright © 2017 Construtor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
